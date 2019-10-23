af1b31acbef783088034a2284404bb84713171af
Felix <felixaviermartinez@gmail.com>
2019-10-23 11:02:58 -0400

    test

--- db/migrate/20191022212600_create_sandwich_ingredients.rb
+++ db/migrate/20191022212600_create_sandwich_ingredients.rb
@@ -1,8 +1,8 @@
 class CreateSandwichIngredients < ActiveRecord::Migration[6.0]
   def change
     create_table :sandwich_ingredients do |t|
-      t.reference :sandwich, null: false, foreign_key: true
-      t.reference :ingredient,null: false, foreign_key: true
+      t.references :sandwich, null: false, foreign_key: true
+      t.references :ingredient,null: false, foreign_key: true
 
       t.timestamps
     end
