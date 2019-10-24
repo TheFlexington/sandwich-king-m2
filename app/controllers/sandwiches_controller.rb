c6b8f1d8a7353cbb2b8b6762a94638cf1f5e37b5
AdamBrownBB <adam.brown.betty@gmail.com>
2019-10-24 14:19:27 -0400

    sandwich ingredient strong params stopping point

--- app/controllers/sandwiches_controller.rb
+++ app/controllers/sandwiches_controller.rb
@@ -16,13 +16,13 @@ class SandwichesController < ApplicationController
     end
 
     def create
-        byebug
-        #stopped here, need to get user working 10/13, 7:30pm
-        @sandwich = Sandwich.create(name: params[:name], price: params[:price])
-        @cookie = Cookie.find(id: params[:cookies])
-        @topping = Topping.find(id: params[:toppings])
-        @ice_cream = IceCream.find(id: params[:ice_creams])
-        @sandwich_ingredient = SandwichIngredient.create(sandwich_id: @sandwich.id, cookie_id: @cookie.id, ice_cream: @ice_cream.id, topping_id: @topping.id,)
+        # byebug
+        @sandwich = Sandwich.create(sandwich_params)
+        @cookie = Cookie.find(params[:sandwich][:cookies])
+        @topping = Topping.find(params[:sandwich][:toppings])
+        @ice_cream = IceCream.find(params[:sandwich][:ice_creams])
+        @sandwich_ingredient = SandwichIngredient.create(sandwich_ingredient_params)
+        # (sandwich_id: @sandwich.id, cookie_id: @cookie.id, ice_cream_id: @ice_cream.id, topping_id: @topping.id,)
        
         if @sandwich.save
             redirect_to sandwich_path(@sandwich)
@@ -30,21 +30,16 @@ class SandwichesController < ApplicationController
             redirect_to new_sandwiches_path
         end
         
-        
-        # @sandwich = Sandwich.create(name: )
-        # ingredient_ids = params[:ingredients] 
-        # ingredient_ids.each do |id|
-        #  @sandwich.sandwich_ingredients.create(ingredient_id: id)
     end # end of create
 
     private
 
     def sandwich_params
-        params.require(:sandwich).permit(:name, :price, :cookies, :ice_creams, :toppings )
+        params.require(:sandwich).permit(:name, :price)
     end
 
-    # def sandwich_ingredient_params
-    #     params.require(:sandwich).permit(:sandwich_id, )
-    # end
+    def sandwich_ingredient_params
+        params.require(:sandwich).permit(:cookie_id, :topping_id, :ice_cream_id )
+    end
 
 end
